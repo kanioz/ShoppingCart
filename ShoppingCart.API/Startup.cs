dcc08aaa4266ec5ec2b3c64053b0e28cc75bfa01
abdulkani.ozbey <abdulkani.ozbey@maysiber.com>
2020-11-09 17:19:16 +0300

    Some tests added

--- ShoppingCart.API/Startup.cs
+++ ShoppingCart.API/Startup.cs
@@ -141,7 +141,7 @@ namespace ShoppingCart.API
 
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ShoppingCart API V1");
             });
         }
     }
--- ShoppingCart.Test/Controller/V1/ShoppingCartControllerTests.cs
+++ ShoppingCart.Test/Controller/V1/ShoppingCartControllerTests.cs
@@ -184,6 +184,108 @@ namespace ShoppingCart.Test.Controller.V1
         }
 
         [Fact]
+        public virtual async Task ChangeQuantity_ShoppingCartItemById()
+        {
+            // Arrange
+            await AuthorizeAsync();
+            var httpResponse = await Client.PostAsync($"/api/{Version}/shoppingcart", null);
+            var httpProductResponse = await Client.GetAsync($"/api/{Version}/product");
+
+            // Must be successful.
+            httpResponse.EnsureSuccessStatusCode();
+            httpProductResponse.EnsureSuccessStatusCode();
+
+            // Deserialize and examine results.
+            var stringResponse = await httpResponse.Content.ReadAsStringAsync();
+            var shoppingCart = JsonConvert.DeserializeObject<Core.Model.ShoppingCart>(stringResponse);
+
+            var stringProductResponse = await httpProductResponse.Content.ReadAsStringAsync();
+            var productList = JsonConvert.DeserializeObject<List<Product>>(stringProductResponse);
+
+            //Assert
+
+            Assert.True(httpResponse.StatusCode == HttpStatusCode.OK);
+            Assert.NotNull(shoppingCart);
+
+            Assert.True(httpProductResponse.StatusCode == HttpStatusCode.OK);
+            Assert.NotNull(productList);
+            Assert.True(productList.Any());
+
+
+            // Arrange
+            httpResponse = await Client.PostAsync($"/api/{Version}/shoppingcart/{shoppingCart.Id}/delete-all-items", null);
+
+            // Must be successful.
+            httpResponse.EnsureSuccessStatusCode();
+
+            // Deserialize and examine results.
+            stringResponse = await httpResponse.Content.ReadAsStringAsync();
+            var shoppingCartItems = JsonConvert.DeserializeObject<List<ShoppingCartItem>>(stringResponse);
+
+            //Assert
+
+            Assert.True(httpResponse.StatusCode == HttpStatusCode.OK);
+            Assert.True(!shoppingCartItems.Any());
+
+
+
+            // Arrange
+            var product = productList.FirstOrDefault(c => c.StockAmount > 0);
+
+            var addRequest = new ShoppingCartAddRequest
+            {
+                ProductId = product?.Id,
+                Quantity = 1
+            };
+            var shoppingCartAddItemContent = new StringContent(JsonConvert.SerializeObject(addRequest), System.Text.Encoding.UTF8, "application/json");
+            httpResponse = await Client.PostAsync($"/api/{Version}/shoppingcart/{shoppingCart.Id}/add-item", shoppingCartAddItemContent);
+
+            // Must be successful.
+            httpResponse.EnsureSuccessStatusCode();
+
+            // Deserialize and examine results.
+            stringResponse = await httpResponse.Content.ReadAsStringAsync();
+            shoppingCart = JsonConvert.DeserializeObject<Core.Model.ShoppingCart>(stringResponse);
+
+            //Assert
+
+            Assert.True(httpResponse.StatusCode == HttpStatusCode.OK);
+            Assert.NotNull(shoppingCart);
+            Assert.NotEmpty(shoppingCart.ShoppingCartItems);
+            Assert.True(shoppingCart.ShoppingCartItems.Count == 1);
+            Assert.True(shoppingCart.TotalPrice == addRequest.Quantity * product?.Price);
+
+
+            // Arrange
+            
+
+            var changeItemQuantityRequestRequest = new ShoppingCartChangeItemQuantityRequest
+            {
+                ShoppingCartItemId = shoppingCart.ShoppingCartItems.FirstOrDefault()?.Id,
+                Quantity = 2
+            };
+            var shoppingCartChangeItemQuantityContent = new StringContent(JsonConvert.SerializeObject(addRequest), System.Text.Encoding.UTF8, "application/json");
+            httpResponse = await Client.PostAsync($"/api/{Version}/shoppingcart/{shoppingCart.Id}/change-item-quantity", shoppingCartChangeItemQuantityContent);
+
+            // Must be successful.
+            httpResponse.EnsureSuccessStatusCode();
+
+            // Deserialize and examine results.
+            stringResponse = await httpResponse.Content.ReadAsStringAsync();
+            shoppingCart = JsonConvert.DeserializeObject<Core.Model.ShoppingCart>(stringResponse);
+
+            //Assert
+
+            Assert.True(httpResponse.StatusCode == HttpStatusCode.OK);
+            Assert.NotNull(shoppingCart);
+            Assert.NotEmpty(shoppingCart.ShoppingCartItems);
+            Assert.True(shoppingCart.ShoppingCartItems.Count == 1);
+            Assert.True(shoppingCart.ShoppingCartItems.FirstOrDefault()?.Quantity == 2);
+            Assert.True(shoppingCart.TotalPrice == changeItemQuantityRequestRequest.Quantity * product?.Price);
+
+        }
+
+        [Fact]
         public virtual async Task Clear_ShoppingCartItemsById()
         {
             // Arrange
